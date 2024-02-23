ae1a87e8f3cf8cf6fc1f11f4d2d907a0c2aad8a0
Ivan <ivanzeng000@gmail.com>
2024-02-24 02:29:12 +0800

    push almost

--- code/login.py
+++ code/login.py
@@ -9,7 +9,7 @@ import re
 import time
 import aiosqlite
 
-fail_types = ('未找到課程','課程不可選','選取失敗','該課程已達人數上限','系統錯誤','年級不可加選！','衝堂不可選！')
+fail_types = ('未找到課程','課程不可選','選取失敗','系統錯誤','年級不可加選！','衝堂不可選！','本類主領域(博雅課程)課程限選2門！')
 success_types = ('本科目設有檢查人數下限。選本課程，在未達下限人數前時無法退選，確定加選?', '成功選取')
 msn = -1
 dialogHandled = asyncio.Event()
@@ -39,6 +39,8 @@ async def handleDialog(dialog):
         msn = 2
     elif text == '帳號或密碼錯誤，請查明後再登入，若您不確定密碼，請執行忘記密碼，取得新密碼後再登入!':
         msn = 3
+    elif '該課程已達人數上限' in text:
+        msn = 4
     else:
         for fail_type in fail_types:
             if fail_type in text:
@@ -250,8 +252,8 @@ async def searchCourse(browser, course):
     all_pages = await browser.pages()
     page = all_pages[0]
     await asyncio.sleep(2.5)
-    await page.waitForSelector('#menuIFrame')
-    await page.waitForSelector('#mainIFrame')
+    menuFrame = await findFrameByName(page, 'menuFrame')
+    mainFrame = await findFrameByName(page, 'mainFrame')
     menuFrame = None; mainFrame = None
     frames = page.frames
     for frame in frames:
@@ -259,20 +261,23 @@ async def searchCourse(browser, course):
             menuFrame = frame
         elif frame.name == 'mainFrame':
             mainFrame = frame 
-    await menuFrame.waitForSelector('#Menu_TreeViewt1')
-    await menuFrame.click('#Menu_TreeViewt1')
-    await menuFrame.waitForSelector('#Menu_TreeViewt31')
-    await menuFrame.click('#Menu_TreeViewt31')
-    await menuFrame.waitForSelector('#Menu_TreeViewt40')
-    await menuFrame.click('#Menu_TreeViewt40')
-    await asyncio.sleep(1.5)
-    await mainFrame.waitForSelector('#Q_CH_LESSON')
-    await mainFrame.evaluate(f"""() => {{document.getElementById('Q_CH_LESSON').value = '{course}';}}""")
-    await mainFrame.waitForSelector('#QUERY_BTN7')
-    await mainFrame.click('#QUERY_BTN7')
 
-    await mainFrame.waitForSelector('#DataGrid tbody tr')
-    trs = await mainFrame.querySelectorAll('#DataGrid tbody tr')
+    selectors_and_frames = [
+        (menuFrame, '#Menu_TreeViewt1'),
+        (menuFrame, '#Menu_TreeViewt31'),
+        (menuFrame, '#Menu_TreeViewt40'),
+        (mainFrame, '#Q_CH_LESSON')]
+
+    for frame, selector in selectors_and_frames:
+        if await waitForSelectorOrTimeout(frame, selector):
+            await frame.click(selector)
+
+    await mainFrame.evaluate(f"""() => {{document.getElementById('Q_CH_LESSON').value = '{course}';}}""")
+    if await waitForSelectorOrTimeout(mainFrame, '#QUERY_BTN7'):
+        await mainFrame.click('#QUERY_BTN7')
+    if await waitForSelectorOrTimeout(mainFrame, '#DataGrid tbody tr'):
+        trs = await mainFrame.querySelectorAll('#DataGrid tbody tr')
+        
     data = []
     # 現在trs應該包含所有的<tr>元素
     for i, tr in enumerate(trs):
@@ -295,7 +300,7 @@ async def searchCourse(browser, course):
         data.append(td_innerText)
     print(data)
     await page.reload()
-    return data, page
+    return data, browser
 
 async def snipeCourse(resnipe, browser, course, which=None):
     global msn
@@ -337,33 +342,30 @@ async def snipeCourse(resnipe, browser, course, which=None):
             tds = await tr.querySelectorAll('td')
             whichClass = await (await tds[3].getProperty('innerText')).jsonValue()
             print(whichClass)
-            if which.upper() == whichClass.upper():
-                onclick = await tds[0].querySelector('a')
-                await onclick.click()
+            
+            if which: 
+                if which.upper() == whichClass.upper():
+                    onclick = await tds[0].querySelector('a')
+                    await onclick.click()
 
     await asyncio.sleep(2)
     tmp = msn; msn = -1
-    if tmp == -1:
-        return False, browser, "未知錯誤"
-    elif tmp == 0:
+    if tmp == -1 or tmp == 0:
         return True, browser, "搶課失敗"
     elif tmp == 1:
         return True, browser, "搶課成功"
+    elif tmp == 4:
+        return True, browser, "人滿"
     #回傳是否可以resnipe, browser
     
 async def main():
     a, b = await login('01157132', 'R125179001')
-    print(b)
-    a,b,c = await snipeCourse(False , a, 'B57020A6')
-    print(c)
-    a,b,c = await snipeCourse(a , b, 'B5701M3J', 'B')
-    print(c)
-    a,b,c = await snipeCourse(a , b, 'B570258M')
-    print(c)
+    a,b =await searchCourse(a, 'asd')
+    print(a)
     b.close()
     #await snipeCourse(a, 'B5701M3J', 'A')
 
-asyncio.get_event_loop().run_until_complete(main())
+#asyncio.get_event_loop().run_until_complete(main())
 
 #使用時間逾時, 系統已將您自動登出, 請再重新登入使用本系統!! <== 掛機alert
 #系統同時一次僅許可一個帳號登入，你已登入過系統，請先登出原帳號再登入!
